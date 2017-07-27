6467c041bf2b19a3092095efbcbbf0250f659a0e
zhiyuan <zhiyuan.zhang@bolemart.com>
2017-07-27 11:46:07 +0800

    3.重写onInterfuptTouchEvent()方法，判断下滑时候拦截事件

--- app/src/main/java/com/zhiyuan/refreshlayout/RefreshLayout.java
+++ app/src/main/java/com/zhiyuan/refreshlayout/RefreshLayout.java
@@ -3,6 +3,7 @@ package com.zhiyuan.refreshlayout;
 import android.content.Context;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 
@@ -11,6 +12,10 @@ import android.widget.ProgressBar;
  */
 
 public class RefreshLayout extends LinearLayout {
+
+    private boolean inrtercept;
+    private double lastY;
+
     public RefreshLayout(Context context) {
         super(context);
         init();
@@ -40,5 +45,29 @@ public class RefreshLayout extends LinearLayout {
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         super.onLayout(changed, l, t, r, b);
         scrollTo(0,getChildAt(0).getHeight());
+    }
+  //  重写onInterfuptTouchEvent()方法，判断下滑时候拦截事件
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        float y = ev.getRawY();
+        switch (ev.getAction()){
+            case  MotionEvent.ACTION_DOWN:
+                inrtercept =false;
+                break;
+
+            case  MotionEvent.ACTION_MOVE:
+                if(y- lastY >2){
+                    inrtercept =true;
+                }else {
+                    inrtercept=false;
+                }
+                break;
+
+            case  MotionEvent.ACTION_UP:
+                  inrtercept=false;
+                break;
+        }
+                   lastY = y;
+            return  inrtercept;
     }
 }
