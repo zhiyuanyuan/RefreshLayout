243d908406e81cc5b7f7483658d846c337efd251
zhiyuan <zhiyuan.zhang@bolemart.com>
2017-07-27 12:37:01 +0800

    4.重写onTouchEvent()方法，处理滑动以及弹性滑动

--- app/src/main/java/com/zhiyuan/refreshlayout/RefreshLayout.java
+++ app/src/main/java/com/zhiyuan/refreshlayout/RefreshLayout.java
@@ -6,6 +6,10 @@ import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.Scroller;
+import android.widget.Toast;
+
+import static android.R.attr.y;
 
 /**
  * Created by admin on 2017/7/27.
@@ -14,7 +18,12 @@ import android.widget.ProgressBar;
 public class RefreshLayout extends LinearLayout {
 
     private boolean inrtercept;
-    private double lastY;
+    private int lastY;
+    private Scroller scroller = new Scroller(getContext());
+    private boolean isRefresh;
+    private int mY;
+    private int yMove;
+    private int i;
 
     public RefreshLayout(Context context) {
         super(context);
@@ -49,14 +58,14 @@ public class RefreshLayout extends LinearLayout {
   //  重写onInterfuptTouchEvent()方法，判断下滑时候拦截事件
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
-        float y = ev.getRawY();
+        mY = (int) ev.getRawY();
         switch (ev.getAction()){
             case  MotionEvent.ACTION_DOWN:
                 inrtercept =false;
                 break;
 
             case  MotionEvent.ACTION_MOVE:
-                if(y- lastY >2){
+                if(mY - lastY >2){
                     inrtercept =true;
                 }else {
                     inrtercept=false;
@@ -67,7 +76,61 @@ public class RefreshLayout extends LinearLayout {
                   inrtercept=false;
                 break;
         }
-                   lastY = y;
+                   lastY = mY;
             return  inrtercept;
     }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        //动画还没结束的时候，直接消耗掉事件,不处理。
+        if (!scroller.isFinished()|| isRefresh) {
+            return true;
+        }
+        mY = (int) event.getY();
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+
+                break;
+            case MotionEvent.ACTION_MOVE:
+                yMove = y - lastY;
+                if (yMove >= 0) {
+                    scrollBy(0, -yMove / 3);   //  /3为了让下拉有感觉
+                    i += yMove / 3;
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                if(i>=getChildAt(0).getHeight()){
+                    smoothToScroll(i-getChildAt(0).getHeight());
+                    i = getChildAt(0).getHeight();
+                    isRefresh = true;
+                    Toast.makeText(getContext(),"正在刷新中",Toast.LENGTH_SHORT).show();
+                }else {
+                    endRefresh();
+                }
+                break;
+        }
+        lastY = y;
+        return true;
+    }
+
+
+    public void endRefresh() {
+        isRefresh = false;
+        smoothToScroll(i);
+        i = 0;
+    }
+    private void smoothToScroll(int destaY) {
+        scroller.startScroll(0, getScrollY(), 0, destaY, 500);
+        invalidate();
+    }
+
+    @Override
+    public void computeScroll() {
+        super.computeScroll();
+        if (scroller.computeScrollOffset()) {
+            scrollTo(0, scroller.getCurrY());
+            postInvalidate();
+        }
+    }
+
 }
